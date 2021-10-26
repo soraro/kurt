5f534de7fa80ea2316bab57d5acae41a9f4f6e13
Aaron <aaron710@gmail.com>
2021-10-26 07:48:40 -0600

    Adding client-go auth plugin

--- cmd/auth.go
+++ cmd/auth.go
@@ -1,11 +1,13 @@
 package cmd
 
 import (
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/clientcmd"
 	"log"
 	"os"
 	"path/filepath"
+
+	"k8s.io/client-go/kubernetes"
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 // Handle setting up cluster auth and return clientset
