e50dab15add46df5bf623b297c16125e4e994604
aro5000 <70827496+aro5000@users.noreply.github.com>
2021-10-26 17:59:06 -0600

    adding kubectl to the help output if the binary is used as kubectl kurt (#10)
    * adding kubectl to the help output if the binary is used as kubectl kurt
    
    * using a template so this works for subcommands

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,10 @@
 package cmd
 
 import (
+	"os"
+	"path/filepath"
+	"strings"
+
 	"github.com/spf13/cobra"
 )
 
@@ -18,10 +22,15 @@ A restart tracker that gives context to what is restarting in your cluster
 }
 
 func init() {
-
 	rootCmd.PersistentFlags().StringSliceVarP(&inamespace, "namespace", "n", []string{""}, "Specify namespace for kurt to collect restart metrics.\nLeave blank to collect in all namespaces.")
 	rootCmd.PersistentFlags().StringSliceVarP(&ilabels, "label", "l", []string{""}, "Specify multiple times for the label keys you want to see.\nFor example: \"kurt all -l app\"")
 	rootCmd.PersistentFlags().IntVarP(&limitFlag, "limit", "c", 5, "Limit the number of resources you want to see. Set limit to 0 for no limits. Must be positive.\nFor example: \"kurt all -c=10\"")
+
+	if strings.HasPrefix(filepath.Base(os.Args[0]), "kubectl-") {
+		rootCmd.SetUsageTemplate(strings.NewReplacer(
+			"{{.UseLine}}", "kubectl {{.UseLine}}",
+			"{{.CommandPath}}", "kubectl {{.CommandPath}}").Replace(rootCmd.UsageTemplate()))
+	}
 
 }
 
