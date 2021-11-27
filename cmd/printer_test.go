a8e73430da3e6d44e16bc652fa14e4c9fb77e6fa
Aaron <aaron710@gmail.com>
2021-11-27 11:22:45 -0700

    adding test

--- cmd/printer_test.go
+++ cmd/printer_test.go
@@ -58,6 +58,48 @@ func Test_returnSortedLimit(t *testing.T) {
 				},
 			},
 		},
+		{
+			name: "test2",
+			args: args{
+				data: map[string]int32{
+					"test1:pod1": 5,
+					"test2:pod2": 7,
+					"test2:pod3": 8,
+					"test1:pod4": 9,
+					"test2:pod5": 0,
+					"test1:pod6": 4,
+				},
+				limit:   5,
+				parseNS: true,
+			},
+			want: ItemList{
+				Item{
+					Name:      "pod4",
+					Count:     9,
+					Namespace: "test1",
+				},
+				Item{
+					Name:      "pod3",
+					Count:     8,
+					Namespace: "test2",
+				},
+				Item{
+					Name:      "pod2",
+					Count:     7,
+					Namespace: "test2",
+				},
+				Item{
+					Name:      "pod1",
+					Count:     5,
+					Namespace: "test1",
+				},
+				Item{
+					Name:      "pod6",
+					Count:     4,
+					Namespace: "test1",
+				},
+			},
+		},
 	}
 
 	for _, tt := range tests {
