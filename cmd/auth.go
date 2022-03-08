296d707f2c4c487aa66dd65089f3c431c172dc75
aro5000 <70827496+aro5000@users.noreply.github.com>
2022-03-08 08:57:58 -0700

    Using the NewNonInteractiveDeferredLoadingClientConfig function to support multiple kubeconfig files (#36)

--- cmd/auth.go
+++ cmd/auth.go
@@ -1,10 +1,6 @@
 package cmd
 
 import (
-	"log"
-	"os"
-	"path/filepath"
-
 	"k8s.io/client-go/kubernetes"
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 	"k8s.io/client-go/tools/clientcmd"
@@ -13,34 +9,20 @@ import (
 // Handle setting up cluster auth and return clientset
 func auth() *kubernetes.Clientset {
 
-	kubeconfig := getConfigPath()
+	loadingRules := clientcmd.NewDefaultClientConfigLoadingRules()
+	configOverrides := &clientcmd.ConfigOverrides{}
 
-	// use the current context in kubeconfig
-	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	kubeConfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(loadingRules, configOverrides)
+	config, err := kubeConfig.ClientConfig()
 	if err != nil {
 		panic(err.Error())
 	}
 
-	// create the clientset
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		panic(err.Error())
 	}
 
 	return clientset
-
-}
-
-func getConfigPath() string {
-
-	if os.Getenv("KUBECONFIG") == "" {
-		homedir, err := os.UserHomeDir()
-		if err != nil {
-			log.Fatal(err)
-		}
-		return filepath.Join(homedir, ".kube", "config")
-	} else {
-		return os.Getenv("KUBECONFIG")
-	}
 
 }
